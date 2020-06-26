de37f4aaf8cee2b1e6a4bf6e07f7ea42115d0914
Haley SuYoun Jeon <tndus0604@hotmail.com>
2020-06-25 23:19:52 -0400

    created config, controller, db, models folders and put the correspoding files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("cheese burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("chicken burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("veggie burger", false);
