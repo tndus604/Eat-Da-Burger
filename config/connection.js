c2371006f3aab6cea6525d6cb4dfebccdbf78325
Haley SuYoun Jeon <tndus0604@hotmail.com>
2020-06-28 17:30:29 -0400

    put password in .env

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 var mysql = require("mysql");
+require('dotenv').config();
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: process.env.PORT || 3306,
     user: "root",
-    password: "hyomi1338",
+    password: process.env.password,
     database: "burgers_db"
 });
 
